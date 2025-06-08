089ca0d10f59de180c3d1339971a2be5d32f10a3
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2025-06-08 22:43:09 +0200

    CORS for REST APIs

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI, Request, status
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
 from slowapi.errors import RateLimitExceeded
 from src.api import contacts, utils, auth, users
 
+
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.exception_handler(RateLimitExceeded)
 async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
@@ -12,11 +23,11 @@ async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
         content={"error": "Request limit exceeded. Please try again later."},
     )
 
+
 app.include_router(utils.router, prefix="/api")
 app.include_router(contacts.router, prefix="/api")
 app.include_router(auth.router, prefix="/api")
 app.include_router(users.router, prefix="/api")
-
 
 
 if __name__ == "__main__":
