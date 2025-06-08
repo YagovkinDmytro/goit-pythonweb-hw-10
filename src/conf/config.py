103f4666fe1b0664a33505f0142c2d406f205c1a
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2025-06-08 19:18:34 +0200

    setting environment variables

--- src/conf/config.py
+++ src/conf/config.py
@@ -1,27 +1,15 @@
-import os
-from pathlib import Path
-from dotenv import load_dotenv
+from pydantic import ConfigDict
+from pydantic_settings import BaseSettings
 
 
-load_dotenv(dotenv_path=Path(__file__).resolve().parent / ".env")
-
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT")
-POSTGRES_DB = os.getenv("POSTGRES_DB")
-JWT_SECRET = os.getenv("JWT_SECRET")
-JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
-JWT_EXPIRATION_SECONDS = os.getenv("JWT_EXPIRATION_SECONDS")
+class Settings(BaseSettings):
+    DB_URL: str
+    JWT_SECRET: str
+    JWT_ALGORITHM: str = "HS256"
+    JWT_EXPIRATION_SECONDS: int = 3600
+    model_config = ConfigDict(
+        extra="ignore", env_file=".env", env_file_encoding="utf-8", case_sensitive=True
+    )
 
 
-# create docker container
-# docker run --name db-contacts -p 5432:5432 -e POSTGRES_USER="POSTGRES_USER" -e POSTGRES_PASSWORD="POSTGRES_PASSWORD" -e POSTGRES_DB="POSTGRES_DB" -d postgres
-
-class Config:
-    DB_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-    JWT_SECRET = JWT_SECRET # Secret key for tokens
-    JWT_ALGORITHM = JWT_ALGORITHM # Token encryption algorithm
-    JWT_EXPIRATION_SECONDS = int(JWT_EXPIRATION_SECONDS) # Token validity period
-
-config = Config
+settings = Settings()
